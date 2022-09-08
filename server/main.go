0fdc6c5a5ac32013b543c9e18b6146611bf0670e
Rakesh <rakeshrakshit740@gmail.com>
2022-09-08 11:53:22 +0530

    server listner created

--- server/main.go
+++ server/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"log"
+	"net"
+
+	pb "projects/Greet/proto"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
+type Server struct {
+	pb.CalculatorServer
+}
+
+var addr string = "0.0.0.0:8192"
+
 func main() {
-	log.Printf("in server main")
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		logrus.Errorf(" error in creating listener %v \n", err)
+	}
+	logrus.Infof("listening on address: %s", addr)
+
+	s := grpc.NewServer()
+
+	if err := s.Serve(lis); err != nil {
+		logrus.Errorf(" error in serving in listener %v \n", err)
+	}
 }
