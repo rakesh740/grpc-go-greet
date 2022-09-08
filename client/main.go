0729c35a29bfbc664076cffd178a93da241fc5ab
Rakesh <rakeshrakshit740@gmail.com>
2022-09-08 12:23:30 +0530

    add client code

--- client/main.go
+++ client/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "0.0.0.0:8192"
 
 func main() {
-	fmt.Print("in client main")
+
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logrus.Errorf(" error in dial connection %s", err)
+	}
+
+	defer conn.Close()
+
+	logrus.Infof("in client main")
+
 }
---
+++ run-client.sh
@@ -0,0 +1,2 @@
+go build -o client ./client
+./client/client
